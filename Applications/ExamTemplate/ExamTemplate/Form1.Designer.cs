5efc42d7b35bfa2af6726388596e9d62f9d8a90a
Andrea Lacava <thecave003@gmail.com>
2018-11-27 17:29:13 +0100

    Added ExamTemplate 	new file:   Applications/ExamTemplate/ExamTemplate.sln 	new file:   Applications/ExamTemplate/ExamTemplate/App.config 	new file:   Applications/ExamTemplate/ExamTemplate/ExamTemplate.csproj 	new file:   Applications/ExamTemplate/ExamTemplate/Form1.Designer.cs 	new file:   Applications/ExamTemplate/ExamTemplate/Form1.cs 	new file:   Applications/ExamTemplate/ExamTemplate/Form1.resx 	new file:   Applications/ExamTemplate/ExamTemplate/Program.cs

---
+++ Applications/ExamTemplate/ExamTemplate/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ExamTemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(15, 39);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series5.Legend = "Legend1";
+            series5.Name = "NormalPDF";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series6.Legend = "Legend1";
+            series6.Name = "NormalCDF";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series7.Legend = "Legend1";
+            series7.Name = "NegExpPDF";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series8.Legend = "Legend1";
+            series8.Name = "NegExpCDF";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(911, 413);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "mu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(137, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "sigmaQ:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(188, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(89, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "N:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(307, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(431, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "lambda:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(481, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 454);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Normal & Negative Exponential Distributions";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Applications/ExamTemplate/ExamTemplate/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamTemplate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // variabile aleatoria distribuzione normale
+        public static double NextGaussianDouble(Random r)
+        {
+            double u, v, S;
+
+            do
+            {
+                u = 2.0 * r.NextDouble() - 1.0;
+                v = 2.0 * r.NextDouble() - 1.0;
+                S = u * u + v * v;
+            }
+            while (S >= 1.0);
+
+            double fac = Math.Sqrt(-2.0 * Math.Log(S) / S);
+            return u * fac;
+        }
+
+        // variabile aleatoria distribuzione negativa exonenziale
+        public static double NextNegativeExpDouble(Random r, double lambda)
+        {
+
+            return Math.Log(1 - r.NextDouble()) / (-lambda);
+        }
+
+        // aggiungi i dati alla normale pdf
+        public void AddDataNormalPDF(double x, double y)
+        {
+            this.chart1.Series["NormalPDF"].Points.AddXY(x, y);
+        }
+
+        // aggiungi i dati alla normale cdf
+        public void AddDataNormalCDF(double x, double y)
+        {
+            this.chart1.Series["NormalCDF"].Points.AddXY(x, y);
+        }
+
+        // aggiungi i dati alla negativa exponenziale
+        public void AddDataNegExpPDF(double x, double y)
+        {
+            this.chart1.Series["NegExpPDF"].Points.AddXY(x, y);
+        }
+
+        public void AddDataNegExpCDF(double x, double y)
+        {
+            this.chart1.Series["NegExpCDF"].Points.AddXY(x, y);
+        }
+
+        private void ClearData()
+        {
+            this.chart1.Series["NormalPDF"].Points.Clear();
+            this.chart1.Series["NormalCDF"].Points.Clear();
+            this.chart1.Series["NegExpPDF"].Points.Clear();
+            this.chart1.Series["NegExpCDF"].Points.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearData();
+            int N = int.Parse(this.textBox3.Text);
+            double mu = double.Parse(this.textBox1.Text);
+            double sigmaQ = double.Parse(this.textBox2.Text);
+            double lambda = double.Parse(this.textBox4.Text);
+            Random rN = new Random();
+            Random rE = new Random();
+            double a, b;
+            for (int i = 0; i < N; i++)
+            {
+                a = NextGaussianDouble(rN);
+                b = NextNegativeExpDouble(rE, lambda);
+                AddDataNormalPDF(a, Normal_func_PDF(a, mu, sigmaQ));
+                AddDataNormalCDF(a, Normal_func_CDF(a, mu, sigmaQ));
+                AddDataNegExpPDF(b, Neg_Exp_func_PDF(b, lambda));
+                AddDataNegExpCDF(b, Neg_Exp_func_CDF(b, lambda));
+            }
+
+        }
+
+        private double Neg_Exp_func_PDF(double b, double lambda)
+        {
+
+            return (b < 0) ? 0.0 : Math.Pow(Math.E, -(lambda * b));
+        }
+
+        private double Neg_Exp_func_CDF(double b, double lambda)
+        {
+
+            return (b < 0) ? 0.0 : 1 - Math.Pow(Math.E, (-(b * lambda)));
+        }
+
+        /*
+        AddDataUBounds(a, nfPDF, sigmaQ);
+        AddDataLBounds(a, nfPDF, sigmaQ);
+        public void AddDataUBounds(double x, double nfPDF, double sigmaQ)
+        {
+            this.chart1.Series["upperBBands"].Points.AddXY(x, nfPDF + sigmaQ);
+        }
+
+        public void AddDataLBounds(double x, double nfPDF, double sigmaQ)
+        {
+            this.chart1.Series["lowerBBands"].Points.AddXY(x, nfPDF - sigmaQ);
+        }
+        */
+
+
+        private static double Normal_func_PDF(double i, double mu, double sigmaQ)
+        {
+            return (1 / Math.Sqrt(2 * Math.PI * sigmaQ)) * Math.Pow(Math.E, -(Math.Pow(i - mu, 2) / (2 * sigmaQ)));
+        }
+
+        static double Normal_func_CDF(double z, double mu, double sigmaQ)
+        {
+            double sigma = Math.Sqrt(sigmaQ);
+
+            double p = 0.3275911;
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+
+            int sign;
+            if ((z - mu) < 0.0)
+                sign = -1;
+            else
+                sign = 1;
+
+            double x = (Math.Abs(z - mu)) / (Math.Sqrt(2.0) * sigma);
+            double t = 1.0 / (1.0 + p * x);
+            double erf = 1.0 - (((((a5 * t + a4) * t) + a3)
+              * t + a2) * t + a1) * t * Math.Exp(-x * x);
+            return 0.5 * (1.0 + sign * erf);
+        }
+    }
+}
