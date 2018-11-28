99514a1859138c5a5ceb88daa865e0cb1003f19f
Andrea Lacava <thecave003@gmail.com>
2018-11-28 13:32:51 +0100

    added past year midterm application 	renamed:    Applications/HelloWorld/HelloWorld/HelloWorld.sln -> Applications/HelloWorld/HelloWorld.sln 	renamed:    Applications/HelloWorld/HelloWorld/HelloWorld/HelloVB.vb -> Applications/HelloWorld/HelloWorld/HelloVB.vb 	renamed:    Applications/HelloWorld/HelloWorld/HelloWorld/HelloWorld.cs -> Applications/HelloWorld/HelloWorld/HelloWorld.cs 	renamed:    Applications/HelloWorld/HelloWorld/HelloWorld/HelloWorld.csproj -> Applications/HelloWorld/HelloWorld/HelloWorld.csproj 	new file:   Applications/PastYearMidTerm/PastYearMidTerm.sln 	new file:   Applications/PastYearMidTerm/PastYearMidTerm/App.config 	new file:   Applications/PastYearMidTerm/PastYearMidTerm/Form1.Designer.cs 	new file:   Applications/PastYearMidTerm/PastYearMidTerm/Form1.cs 	new file:   Applications/PastYearMidTerm/PastYearMidTerm/Form1.resx 	new file:   Applications/PastYearMidTerm/PastYearMidTerm/PastYearMidTerm.csproj 	new file:   Applications/PastYearMidTerm/PastYearMidTerm/Program.cs

---
+++ Applications/PastYearMidTerm/PastYearMidTerm/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ExamTemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series21 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series22 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series23 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series24 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(15, 39);
+            this.chart1.Name = "chart1";
+            series19.ChartArea = "ChartArea1";
+            series19.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series19.Legend = "Legend1";
+            series19.Name = "NormalPDF";
+            series20.ChartArea = "ChartArea1";
+            series20.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series20.Legend = "Legend1";
+            series20.Name = "NormalCDF";
+            series21.ChartArea = "ChartArea1";
+            series21.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series21.Legend = "Legend1";
+            series21.Name = "SubNormalPDF";
+            series22.ChartArea = "ChartArea1";
+            series22.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series22.Legend = "Legend1";
+            series22.Name = "SubNormalCDF";
+            series23.ChartArea = "ChartArea1";
+            series23.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series23.Legend = "Legend1";
+            series23.Name = "NegExpPDF";
+            series24.ChartArea = "ChartArea1";
+            series24.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series24.Legend = "Legend1";
+            series24.Name = "NegExpCDF";
+            this.chart1.Series.Add(series19);
+            this.chart1.Series.Add(series20);
+            this.chart1.Series.Add(series21);
+            this.chart1.Series.Add(series22);
+            this.chart1.Series.Add(series23);
+            this.chart1.Series.Add(series24);
+            this.chart1.Size = new System.Drawing.Size(911, 413);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "N:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(516, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "lambda:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(566, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "AddSubDataToSample";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(274, 13);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "%:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(672, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Generate Exponential";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 454);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Normal Distributions";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Applications/PastYearMidTerm/PastYearMidTerm/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamTemplate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // variabile aleatoria distribuzione normale
+        public static double NextGaussianDouble(Random r)
+        {
+            double u, v, S;
+
+            do
+            {
+                u = 2.0 * r.NextDouble() - 1.0;
+                v = 2.0 * r.NextDouble() - 1.0;
+                S = u * u + v * v;
+            }
+            while (S >= 1.0);
+
+            double fac = Math.Sqrt(-2.0 * Math.Log(S) / S);
+            return u * fac;
+        }
+
+        // variabile aleatoria distribuzione negativa exonenziale
+        public static double NextNegativeExpDouble(Random r, double lambda)
+        {
+
+            return Math.Log(1 - r.NextDouble()) / (-lambda);
+        }
+
+        // aggiungi i dati alla normale pdf
+        public void AddDataNormalPDF(double x, double y)
+        {
+            this.chart1.Series["NormalPDF"].Points.AddXY(x, y);
+        }
+
+        // aggiungi i dati alla normale cdf
+        public void AddDataNormalCDF(double x, double y)
+        {
+            this.chart1.Series["NormalCDF"].Points.AddXY(x, y);
+        }
+
+        // aggiungi i dati alla normale pdf
+        public void AddDataSubNormalPDF(double x, double y)
+        {
+            this.chart1.Series["SubNormalPDF"].Points.AddXY(x, y);
+        }
+
+        // aggiungi i dati alla normale cdf
+        public void AddDataSubNormalCDF(double x, double y)
+        {
+            this.chart1.Series["SubNormalCDF"].Points.AddXY(x, y);
+        }
+        // aggiungi i dati alla negativa exponenziale
+        public void AddDataNegExpPDF(double x, double y)
+        {
+            this.chart1.Series["NegExpPDF"].Points.AddXY(x, y);
+        }
+
+        public void AddDataNegExpCDF(double x, double y)
+        {
+            this.chart1.Series["NegExpCDF"].Points.AddXY(x, y);
+        }
+
+        private void ClearData()
+        {
+            this.chart1.Series["NormalPDF"].Points.Clear();
+            this.chart1.Series["NormalCDF"].Points.Clear();
+            this.chart1.Series["NegExpPDF"].Points.Clear();
+            this.chart1.Series["NegExpCDF"].Points.Clear();
+            ClearSubData();
+        }
+
+        private void ClearSubData()
+        {
+            this.chart1.Series["SubNormalPDF"].Points.Clear();
+            this.chart1.Series["SubNormalCDF"].Points.Clear();
+        }
+
+        double[] samples;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ClearData();
+            int N = int.Parse(this.textBox3.Text);
+            Random rN = new Random();
+            double a;
+            samples = new double[N];
+            // Genero la distribuzione completa di N elementi e la plotto
+            for (int i = 0; i < N; i++)
+            {
+                a = NextGaussianDouble(rN);
+                //b = NextNegativeExpDouble(rE, lambda);
+                samples[i] = a;
+                AddDataNormalPDF(a, Normal_func_PDF(a, 0, 1));
+                AddDataNormalCDF(a, Normal_func_CDF(a, 0, 1));
+                //AddDataNegExpPDF(b, Neg_Exp_func_PDF(b, lambda));
+                //AddDataNegExpCDF(b, Neg_Exp_func_CDF(b, lambda));
+            }
+
+        }
+
+        // Genero la distribuzione del 10% degli elementi costruiti prima, ci calcolo media e varianza e li plotto
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (samples is null)
+            {
+                MessageBox.Show("Premi Generate prima");
+                return;
+            }
+            ClearSubData();
+            double percentage = double.Parse(this.textBox5.Text) / 100;
+            double[] subSamples = new double[(int)(samples.Length * percentage)];
+            Random r = new Random();
+
+            // popolo subSamples
+            // OVVIAMENTE QUESTA FUNZIONE DI POPOLAMENTO NON è AL 100% CORRETTA, 
+            // può capitare che alcuni dati vengano riproposti una migliore implementazione prevede
+            // un uso di liste, ma sinceramente pish
+            for (int i = 0; i < subSamples.Length; i++)
+            {
+                subSamples[i] = samples[r.Next(0, samples.Length - 1)];
+            }
+
+            // calcolo media e varianza ridotti
+            double subMu;
+            double subSigmaQ = 0.0;
+            double sumSamples = 0.0;
+            for (int i = 0; i < subSamples.Length; i++)
+            {
+                sumSamples += subSamples[i];
+            }
+            subMu = sumSamples / subSamples.Length;
+
+            double differences = 0.0;
+            for (int i = 0; i < subSamples.Length; i++)
+            {
+                differences += Math.Pow(subSamples[i] - subMu, 2);
+            }
+
+            subSigmaQ = differences / subSamples.Length;
+
+            // plot distribuzione campione ridotto
+            for (int i = 0; i < subSamples.Length; i++)
+            {
+                AddDataSubNormalPDF(subSamples[i], Normal_func_PDF(subSamples[i], subMu, subSigmaQ));
+                AddDataSubNormalCDF(subSamples[i], Normal_func_CDF(subSamples[i], subMu, subSigmaQ));
+            }
+
+        }
+
+
+        static double Neg_Exp_func_PDF(double b, double lambda)
+        {
+
+            return (b < 0) ? 0.0 : Math.Pow(Math.E, -(lambda * b));
+        }
+
+        static double Neg_Exp_func_CDF(double b, double lambda)
+        {
+
+            return (b < 0) ? 0.0 : 1 - Math.Pow(Math.E, (-(b * lambda)));
+        }
+
+        static double Normal_func_PDF(double i, double mu, double sigmaQ)
+        {
+            return (1 / Math.Sqrt(2 * Math.PI * sigmaQ)) * Math.Pow(Math.E, -(Math.Pow(i - mu, 2) / (2 * sigmaQ)));
+        }
+
+        static double Normal_func_CDF(double z, double mu, double sigmaQ)
+        {
+            double sigma = Math.Sqrt(sigmaQ);
+
+            double p = 0.3275911;
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+
+            int sign;
+            if ((z - mu) < 0.0)
+                sign = -1;
+            else
+                sign = 1;
+
+            double x = (Math.Abs(z - mu)) / (Math.Sqrt(2.0) * sigma);
+            double t = 1.0 / (1.0 + p * x);
+            double erf = 1.0 - (((((a5 * t + a4) * t) + a3)
+              * t + a2) * t + a1) * t * Math.Exp(-x * x);
+            return 0.5 * (1.0 + sign * erf);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (samples is null)
+            {
+                MessageBox.Show("Premi Generate prima");
+                return;
+            }
+
+            double lambda = double.Parse(this.textBox4.Text);
+            Random rE = new Random();
+            double b;
+            int N = int.Parse(this.textBox3.Text);
+
+            for (int i = 0; i < N; i++)
+            {
+                b = NextNegativeExpDouble(rE, lambda);
+                AddDataNegExpPDF(b, Neg_Exp_func_PDF(b, lambda));
+                AddDataNegExpCDF(b, Neg_Exp_func_CDF(b, lambda));
+            }
+        }
+
+        /*
+        AddDataUBounds(a, nfPDF, sigmaQ);
+        AddDataLBounds(a, nfPDF, sigmaQ);
+        public void AddDataUBounds(double x, double nfPDF, double sigmaQ)
+        {
+            this.chart1.Series["upperBBands"].Points.AddXY(x, nfPDF + sigmaQ);
+        }
+
+        public void AddDataLBounds(double x, double nfPDF, double sigmaQ)
+        {
+            this.chart1.Series["lowerBBands"].Points.AddXY(x, nfPDF - sigmaQ);
+        }
+        */
+    }
+}
