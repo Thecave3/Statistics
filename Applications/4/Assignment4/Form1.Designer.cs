6bc4d871482160f462b2a58a8f4ca9aecca0e66b
Andrea Lacava <thecave003@gmail.com>
2018-12-10 16:03:41 +0100

    Completata Assignment 4o 	modified:   Applications/4/Assignment4/Form1.Designer.cs 	modified:   Applications/4/Assignment4/Form1.cs 	new file:   Applications/4/Assignment4/Properties/AssemblyInfo.cs 	new file:   Applications/4/Assignment4/Properties/Resources.Designer.cs 	new file:   Applications/4/Assignment4/Properties/Resources.resx 	new file:   Applications/4/Assignment4/Properties/Settings.Designer.cs 	new file:   Applications/4/Assignment4/Properties/Settings.settings

--- Applications/4/Assignment4/Form1.Designer.cs
+++ Applications/4/Assignment4/Form1.Designer.cs
@@ -31,16 +31,16 @@ namespace Assignment4
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label3 = new System.Windows.Forms.Label();
+            this.populationLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,9 +49,9 @@ namespace Assignment4
             this.label1.Location = new System.Drawing.Point(33, 21);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(21, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Size: ";
+            this.label1.Text = "N: ";
             // 
             // label2
             // 
@@ -63,24 +63,6 @@ namespace Assignment4
             this.label2.TabIndex = 1;
             this.label2.Text = "0";
             // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(35, 186);
-            this.chart1.Margin = new System.Windows.Forms.Padding(2);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Medie";
-            series1.Points.Add(dataPoint1);
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(1179, 534);
-            this.chart1.TabIndex = 2;
-            this.chart1.Text = "Sampling Distribution of the mean";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -91,30 +73,91 @@ namespace Assignment4
             this.label3.TabIndex = 3;
             this.label3.Text = "Population:";
             // 
+            // populationLabel
+            // 
+            this.populationLabel.AutoSize = true;
+            this.populationLabel.Location = new System.Drawing.Point(95, 8);
+            this.populationLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.populationLabel.Name = "populationLabel";
+            this.populationLabel.Size = new System.Drawing.Size(18, 13);
+            this.populationLabel.TabIndex = 4;
+            this.populationLabel.Text = "{ }";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(36, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(414, 415);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(456, 38);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(535, 415);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Subsets";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(738, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Combinations";
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(95, 8);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(98, 21);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(18, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "{ }";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "K:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(122, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 425);
+            this.ClientSize = new System.Drawing.Size(1003, 465);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.populationLabel);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.chart1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Assignment4";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,9 +167,14 @@ namespace Assignment4
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label populationLabel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Applications/4/Assignment4/Form1.cs
+++ Applications/4/Assignment4/Form1.cs
@@ -12,13 +12,15 @@ namespace Assignment4
 {
     public partial class Form1 : Form
     {
-        List<int> population;
+        int[] population;
+
+        int K = 3;
 
         public Form1(List<int> population)
         {
-            this.population = population;
+            this.population = population.ToArray();
             InitializeComponent();
-            this.chart1.ChartAreas[0].AxisY.Interval = 1;
+            //     this.chart1.ChartAreas[0].AxisY.Interval = 1;
             GenerateSamples();
             // no smaller than design time size
             this.MinimumSize = new Size(this.Width, this.Height);
@@ -26,35 +28,65 @@ namespace Assignment4
             this.MaximumSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             this.AutoSize = true;
             this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            this.AddDistributionToChart();
-
+            this.populationLabel.Text = "{";
+            foreach (int i in population)
+            {
+                this.populationLabel.Text += " " + i;
+            }
+            this.populationLabel.Text += " }";
+            this.label2.Text = population.Count.ToString();
+            this.label7.Text = K.ToString();
         }
+
+        // Subset: Disposizioni
+        // Combinations: Combinazioni
 
         public void GenerateSamples()
         {
+            List<string> result = new List<string>();
+            Subsets(population, K, 0, 0, "", ref result);
+            richTextBox1.Text += "Number of Subsets: " + result.Count() + Environment.NewLine;
 
-            List<List<int[]>> combinations = new List<List<int[]>>();
+            foreach (var item in result)
+                richTextBox1.Text += item + Environment.NewLine;
 
-            for (int n = 2; n < population.Count; n++)
+            result.Clear();
+
+            Combinations(population, K, 0, 0, "", ref result);
+
+            richTextBox2.Text += "Number of Combinations: " + resultComb.Count() + Environment.NewLine;
+
+            foreach (var item in resultComb)
+                richTextBox2.Text += item + Environment.NewLine;
+        }
+
+        // Disposizioni con ripetizione n^k
+        void Subsets(int[] population, int n, int j, int k, string appo, ref List<string> result)
+        {
+            if (j >= n)
+                result.Add(appo);
+            else
+                for (int i = 0; i < population.Length; i++)
+                    Subsets(population, n, j + 1, i, appo + population[i] + " ", ref result);
+        }
+
+        // Combinazioni senza ripetizione
+        List<string> resultComb = new List<string>();
+        void Combinations(int[] population, int n, int j, int k, string appo, ref List<string> result)
+        {
+            if (j == n)
             {
-               // combinations.Add();
-                
-               
+                // richTextBox2.Text += appo + " " + Environment.NewLine;
+                resultComb.Add(appo);
             }
-
-            AddDataToChart(combinations);
+            else
+                for (int i = k; i < population.Length; i++)
+                    Combinations(population, n, j + 1, i + 1, appo + population[i] + " ", ref result);
         }
 
-       
-    
+        /*
 
-    public void AddDataToChart(List<List<int[]>> comb)
-    {
-          
-        }
-
-
-    public void AddDistributionToChart()
+        public void AddDistributionToChart()
     {
         // Distribuzione della popolazione
 
@@ -92,8 +124,8 @@ namespace Assignment4
         this.chart1.ChartAreas[0].AxisX.Minimum = min - 1;
         this.chart1.Invalidate();
     }
+    */
 
-
-}
+    }
 }
 
