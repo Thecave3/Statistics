a89fd91f856277b623d945c1be4f6236bfd5dd55
Andrea Lacava <thecave003@gmail.com>
2018-12-10 14:42:29 +0100

    new file:   Applications/MidTerm/MidTerm.sln 	new file:   Applications/MidTerm/MidTerm/App.config 	new file:   Applications/MidTerm/MidTerm/Form1.Designer.cs 	new file:   Applications/MidTerm/MidTerm/Form1.cs 	new file:   Applications/MidTerm/MidTerm/Form1.resx 	new file:   Applications/MidTerm/MidTerm/MidTerm.csproj 	new file:   Applications/MidTerm/MidTerm/Program.cs 	new file:   Applications/MidTerm/MidTerm/Properties/AssemblyInfo.cs 	new file:   Applications/MidTerm/MidTerm/Properties/Resources.Designer.cs 	new file:   Applications/MidTerm/MidTerm/Properties/Resources.resx 	new file:   Applications/MidTerm/MidTerm/Properties/Settings.Designer.cs 	new file:   Applications/MidTerm/MidTerm/Properties/Settings.settings 	new file:   Applications/NegativeExponential/NegativeExponential.sln 	new file:   Applications/NegativeExponential/NegativeExponential/App.config 	new file:   Applications/NegativeExponential/NegativeExponential/Form1.Designer.cs 	new file:   Applications/NegativeExponential/NegativeExponential/Form1.cs 	new file:   Applications/NegativeExponential/NegativeExponential/Form1.resx 	new file:   Applications/NegativeExponential/NegativeExponential/NegativeExponential.csproj 	new file:   Applications/NegativeExponential/NegativeExponential/Program.cs 	new file:   Applications/NegativeExponential/NegativeExponential/Properties/AssemblyInfo.cs 	new file:   Applications/NegativeExponential/NegativeExponential/Properties/Resources.Designer.cs 	new file:   Applications/NegativeExponential/NegativeExponential/Properties/Resources.resx 	new file:   Applications/NegativeExponential/NegativeExponential/Properties/Settings.Designer.cs 	new file:   Applications/NegativeExponential/NegativeExponential/Properties/Settings.settings

---
+++ Applications/NegativeExponential/NegativeExponential/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace NegativeExponential
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelN = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N:";
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(15, 33);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.IsVisibleInLegend = false;
+            series4.Legend = "Legend1";
+            series4.Name = "NegExpAvg";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(764, 454);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(36, 9);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(13, 13);
+            this.labelN.TabIndex = 8;
+            this.labelN.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 499);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "NegativeExponentialMeanDistribution";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Applications/NegativeExponential/NegativeExponential/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NegativeExponential
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new Timer
+            {
+                Interval = 1000
+            };
+            timer.Tick += new System.EventHandler(Routine);
+        }
+
+        private void Routine(object sender, EventArgs e)
+        {
+            ClearData();
+            this.labelN.Text = N.ToString();
+            List<double> means = new List<double>();
+
+            for (int i = 0; i < iteration_factor; i++)
+            {
+                means.Add(GenerateDistributionAndExtractMean(r));
+            }
+            means.Max();
+
+
+            Dictionary<double, int> frequencies = new Dictionary<double, int>();
+            double standard;
+            foreach (double mean in means)
+            {
+                standard = Math.Floor((mean - (means.Max() - means.Min())) /((means.Max() - means.Min()/50)));
+                if (frequencies.ContainsKey(standard))
+                    frequencies[standard] += 1;
+                else
+                    frequencies[standard] = 1;
+            }
+            AddDataToChart(frequencies);
+            N++;
+        }
+
+
+        double lambda = 2.0;
+        int iteration_factor = 500;
+        int N = 1;
+        Random r = new Random();
+        Timer timer;
+
+        // Generatore di distribuzione negativa esponenziale
+        public static double NextNegativeExpDouble(Random r, double lambda)
+        {
+            return Math.Log(1 - r.NextDouble()) / (-lambda);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearData();
+            timer.Start();
+        }
+
+        private void AddDataToChart(Dictionary<double, int> frequencies)
+        {
+            foreach (double key in frequencies.Keys)
+            {
+                this.chart1.Series["NegExpAvg"].Points.AddXY(key, frequencies[key]);
+            }
+        }
+
+        private double GenerateDistributionAndExtractMean(Random r)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < 5000; i++)
+            {
+                sum += NextNegativeExpDouble(r, lambda);
+            }
+            return sum / N;
+        }
+
+
+
+        private void ClearData()
+        {
+            this.chart1.Series["NegExpAvg"].Points.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+    }
+}
